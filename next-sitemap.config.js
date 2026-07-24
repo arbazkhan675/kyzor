f307e6bc058e2330dd6b0b07bdf2fccf5b4b7c21
ArbazKhan121 <arbazkhan886493@gmail.com>
2026-07-24 16:46:56 +0530

    Clean workspace - delete all frontend and backend code

--- next-sitemap.config.js
+++
@@ -1,8 +0,0 @@
-/** @type {import('next-sitemap').IConfig} */
-module.exports = {
-    siteUrl: 'https://www.kyzor.online',
-    generateRobotsTxt: true,
-    changefreq: 'weekly',
-    priority: 0.7,
-    exclude: ['/login', '/dashboard/*', '/admin/*'],
-}
